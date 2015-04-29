4c98fe63a91b8dc8201d40187c393c805b7bfffb
Alex Kaiser <atkaiser@berkeley.edu>
2015-04-28 20:33:14 -0700

    Update wcs configs

--- sc2game/utils/create_config.py
+++ sc2game/utils/create_config.py
@@ -3,11 +3,12 @@ import cv2
 import cv2.cv as cv
 import sys
 
-if len(sys.argv) != 2:
-    print "Use create_config.py <image to test on>"
+if len(sys.argv) != 3:
+    print "Use create_config.py <image to test on> <resolution>"
     sys.exit()
 
 im_path = sys.argv[1]
+res = sys.argv[2]
 
 boxes = []
 box_name = raw_input()
@@ -15,12 +16,12 @@ box_name = raw_input()
 def on_mouse(event, x, y, flags, params):
     global box_name
     if event == cv.CV_EVENT_LBUTTONDOWN:
-        print box_name + "_l=" + str(x)
-        print box_name + "_u=" + str(y)
+        print box_name + "_l_" + res + "=" + str(x)
+        print box_name + "_u_" + res + "=" + str(y)
 
     elif event == cv.CV_EVENT_LBUTTONUP:
-        print box_name + "_r=" + str(x)
-        print box_name + "_d=" + str(y)
+        print box_name + "_r_" + res + "=" + str(x)
+        print box_name + "_d_" + res + "=" + str(y)
         box_name = raw_input()
 
 
