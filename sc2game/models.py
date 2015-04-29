9d367e41fa1786fdc15a83e52f4072e1d0a631bd
Alex Kaiser <atkaiser@berkeley.edu>
2015-04-28 20:47:49 -0700

    Adding str methods to models.

--- sc2game/models.py
+++ sc2game/models.py
@@ -6,7 +6,9 @@ class Stream(models.Model):
     name = models.CharField(max_length=100, default = "")
     url = models.CharField(max_length=100, default = "")
     up = models.BooleanField(default = False)
-
+    
+    def __str__(self):
+        return self.name
 
 class Game(models.Model):
     game_on = models.BooleanField(default = False)
@@ -16,6 +18,8 @@ class Game(models.Model):
     current_round = models.CharField(max_length=100)
     stream = models.ForeignKey(Stream)
 
+    def __str__(self):
+        return "Game for stream: " + self.stream.name
 
 class Player(models.Model):
     name = models.CharField(max_length=100, default = "")
@@ -28,7 +32,12 @@ class Player(models.Model):
     army = models.IntegerField(default=0)
     stream = models.ForeignKey(Stream)
 
+    def __str__(self):
+        return "Player for stream: " + self.stream.name
 
 class Bracket(models.Model):
     url = models.CharField(max_length=100, default = "")
     stream = models.ForeignKey(Stream)
+    
+    def __str__(self):
+        return "Bracket for stream: " + self.stream.name
