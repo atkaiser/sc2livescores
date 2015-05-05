357def25a8dedae348b600031d0a2df3477929e7
Alex Kaiser <atkaiser@berkeley.edu>
2015-05-05 00:42:49 -0700

    Fixing deploy script.

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -9,7 +9,7 @@ ssh sc2ls@sc2ls.mooo.com /bin/bash << EOF
   ps aux | grep update_state.py | sed 's/\s\+/ /g' | cut -d' ' -f2 | xargs kill -9
   cd /home/sc2ls/dev/sc2livescores
   git pull
-  mpy collectstatic --noinput
+  python manage.py collectstatic --noinput
   echo "Starting server ..."
   nohup uwsgi --ini sc2livescores_uwsgi.ini > logs/server.out 2> logs/server.err < /dev/null &
   cd sc2game/utils
