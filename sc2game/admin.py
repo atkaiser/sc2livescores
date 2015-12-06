28f96759b084bc908169e0e1bd09c8ddb6fe66a6
Alex Kaiser <atkaiser@berkeley.edu>
2015-12-06 12:03:43 -0800

    Adding stuff to admin page

--- sc2game/admin.py
+++ sc2game/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from sc2game.models import Stream, Game, Player, Bracket
+
+admin.site.register(Stream)
+admin.site.register(Game)
+admin.site.register(Player)
+admin.site.register(Bracket)
+
 # Register your models here.
