146ce4738ac14d6f14e81c2d07865fb69480f9a8
Alex Kaiser <atkaiser@berkeley.edu>
2015-12-06 13:48:57 -0800

    Adding back in taketv

--- sc2game/utils/test_image.py
+++ sc2game/utils/test_image.py
@@ -1,13 +1,27 @@
 import update_state
 import sys
+import ConfigParser
+from sc2livescores import sets
 from PIL import Image
 
-if len(sys.argv) != 3:
-    print "Use test_image.py <image to test on> <stream name>"
+if len(sys.argv) < 2:
+    print "Use test_image.py <stream name> <image to test on>"
     sys.exit()
-    
-im = Image.open(sys.argv[1])
-my_data = update_state.get_data_from_image(im, update_state.parser, sys.argv[2])
+
+section_name = sys.argv[1]
+
+if len(sys.argv) == 3:
+    im = Image.open(sys.argv[2])
+else:
+    parser = ConfigParser.ConfigParser()
+    parser.read(sets.conf_file)
+    stream_url = stream_url = parser.get(section_name, 'stream_url')
+    stream = update_state.get_stream(stream_url)
+    im = update_state.get_screenshot(stream, section_name)
+
+resolution = im.size[1]
+print "resolution: " + str(resolution) + " for section: " + section_name
+my_data = update_state.get_data_from_image(im, update_state.parser, section_name)
 
 for key in my_data.keys():
     print str(key) + ": " + str(my_data[key])
