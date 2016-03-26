d668744901bcd9de63eaee61f562595aa7be205c
Alex Kaiser <atkaiser@berkeley.edu>
2016-03-25 23:38:00 -0700

    Adding in proleague

--- sc2game/utils/create_config.py
+++ sc2game/utils/create_config.py
@@ -1,6 +1,5 @@
 import numpy as np
 import cv2
-import cv2.cv as cv
 import sys
 
 if len(sys.argv) != 3:
@@ -15,13 +14,13 @@ box_name = raw_input()
 
 def on_mouse(event, x, y, flags, params):
     global box_name
-    if event == cv.CV_EVENT_LBUTTONDOWN:
+    if event == cv2.EVENT_LBUTTONDOWN:
         print box_name + "_l_" + res + "=" + str(x)
         print box_name + "_u_" + res + "=" + str(y)
-
-    elif event == cv.CV_EVENT_LBUTTONUP:
+    elif event == cv2.EVENT_LBUTTONUP:
         print box_name + "_r_" + res + "=" + str(x)
         print box_name + "_d_" + res + "=" + str(y)
+        print ""
         box_name = raw_input()
 
 
@@ -29,7 +28,7 @@ count = 0
 
 img = cv2.imread(im_path, 0)
 cv2.namedWindow('real image')
-cv.SetMouseCallback('real image', on_mouse, 0)
+cv2.setMouseCallback('real image', on_mouse, 0)
 cv2.imshow('real image', img)
 while(True):
     count += 1
