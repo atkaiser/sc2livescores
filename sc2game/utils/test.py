19d997d01f85c5798569542c88d37045dbf2590e
Alex Kaiser <atkaiser@berkeley.edu>
2016-05-14 18:17:23 -0700

    Update to create config

--- sc2game/utils/test.py
+++ sc2game/utils/test.py
@@ -5,13 +5,30 @@ Created on Jul 16, 2014
 '''
 
 import sys
+import cv2
 
-with open(sys.argv[1]) as f:
-    for line in f:
-        l = line.rstrip()
-        if "=" in l:
-            num = l.index("=")
-            print l[0:num] + "_1080" + l[num:]
-        else:
-            print l
+if __name__ == '__main__':
+    im_path = sys.argv[1]
+    img = cv2.imread(im_path, 0)
+    mser = cv2.MSER_create()
+#     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    vis = img.copy()
 
+    regions = mser.detectRegions(img, None)
+    hulls = [cv2.convexHull(p.reshape(-1, 1, 2)) for p in regions]
+    cv2.polylines(vis, hulls, 1, (0, 255, 0))
+
+    cv2.imshow('img', vis)
+    count = 0
+    while(True):
+        count += 1
+    
+        if count < 50:
+            if cv2.waitKey(33) == 27:
+                cv2.destroyAllWindows()
+                break
+        elif count >= 50:
+            if cv2.waitKey(0) == 27:
+                cv2.destroyAllWindows()
+                break
+            count = 0
