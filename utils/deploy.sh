d3059ca8113c834a9f895681a903d09e64884d56
Alex Kaiser <atkaiser@berkeley.edu>
2017-01-16 09:20:50 -0800

    Updating deploy script

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -3,7 +3,7 @@ git commit
 git push origin master
 
 # ssh and setup environ
-ssh sc2ls@sc2ls.mooo.com /bin/bash << EOF
+ssh akaiser@akaiser0.mooo.com /bin/bash << EOF
   source /home/sc2ls/.bash_profile
   ps aux | grep uwsgi | grep sc2livescores | grep -v grep | sed 's/\s\+/ /g' | cut -d' ' -f2 | xargs kill -9
   ps aux | grep update_state.py | sed 's/\s\+/ /g' | cut -d' ' -f2 | xargs kill -9
@@ -12,7 +12,7 @@ ssh sc2ls@sc2ls.mooo.com /bin/bash << EOF
   git pull
   python manage.py collectstatic --noinput
   echo "Starting server ..."
-  nohup uwsgi --ini sc2livescores_uwsgi.ini > logs/server.out 2> logs/server.err < /dev/null &
+  nohup uwsgi --ini sc2livescores_uwsgi.ini > /var/log/sc2ls/server/server.out 2>&1 < /dev/null &
   cd sc2game/utils
   echo "Starting update_state process ..."
   nohup python -u update_state.py &> ../../logs/update.log < /dev/null &
