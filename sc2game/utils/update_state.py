acd02c9a713f0fe7f493e657d981ad6223cd41e8
Alex Kaiser <atkaiser@berkeley.edu>
2017-01-20 08:23:42 -0800

    Adding rebroadcast to list of not live streams

--- sc2game/utils/update_state.py
+++ sc2game/utils/update_state.py
@@ -270,7 +270,7 @@ def get_info_from_stream(section_name):
                 stream_obj.save()
                 time.sleep(60)
                 continue
-            if "rerun" in json_res["status"].lower():
+            if any(x in json_res["status"].lower() for x in ["rerun", "rebroadcast"]):
                 logger.info("Showing rerun in stream: " + stream_url)
                 stream_obj.up = False
                 stream_obj.save()
