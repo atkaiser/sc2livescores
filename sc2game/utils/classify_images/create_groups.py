6fbfe8e6adf3b331319eea5fbf4ffc65484a09ec
Alex Kaiser <atkaiser@berkeley.edu>
2017-04-07 21:06:13 -0700

    Creating classify_images dir

---
+++ sc2game/utils/classify_images/create_groups.py
@@ -0,0 +1,15 @@
+'''
+Created on Jun 5, 2016
+
+Be able to find out what label each group from kmeans should be.
+
+@author: akaiser
+'''
+
+def main():
+    input = raw_input()
+    while input:
+        int(input)
+
+if __name__ == '__main__':
+    main()
