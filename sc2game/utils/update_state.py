95049982608be795cffcfe653f42b585f48b8134
Alex Kaiser <alextkaiser@gmail.com>
2017-12-06 08:28:33 -0800

    Adding objgraph output to update_state

--- sc2game/utils/update_state.py
+++ sc2game/utils/update_state.py
@@ -1,27 +1,29 @@
-import sys
+import ConfigParser
+from cStringIO import StringIO
+import errno
+import gc
+import logging
 import os
+import pdb
+import random
+import re
+from shutil import copyfile
+import string
+import subprocess
+import sys
+import threading
+import time
+
+from PIL import Image
+from django.utils import timezone
+from livestreamer import Livestreamer
+import objgraph
+import requests
 sys.path.append(os.environ['SC2LS_PATH'])
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sc2livescores.settings")
+from sc2game.models import Game, Player, Stream, Bracket
 from sc2livescores import sets
 from sc2livescores import settings
-from PIL import Image
-import time
-import ConfigParser
-import subprocess
-import errno
-from livestreamer import Livestreamer
-import threading
-import re
-from django.utils import timezone
-from sc2game.models import Game, Player, Stream, Bracket
-import logging
-import requests
-import string
-import random
-from shutil import copyfile
-import objgraph
-import gc
-import pdb
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -29,7 +31,8 @@ handler = logging.handlers.RotatingFileHandler(os.path.join(settings.LOG_DIR, 'u
                                                maxBytes=50 * 1024 * 1024,
                                                backupCount=5)
 handler.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
@@ -40,13 +43,15 @@ image_temp_file = "../temps/"
 parser = ConfigParser.ConfigParser()
 parser.read(sets.conf_file)
 
+
 def mkdir_p(path):
     try:
         os.makedirs(path)
-    except OSError as exc: # Python >2.5
+    except OSError as exc:  # Python >2.5
         if exc.errno == errno.EEXIST and os.path.isdir(path):
             pass
-        else: raise
+        else:
+            raise
 
 
 def threshold(im, section_name, pic):
@@ -93,28 +98,30 @@ def get_screenshot(stream, section_name):
     logger.debug("Call to FFMPEG: " + str(command))
     subprocess.call(command, stdout=FNULL, stderr=subprocess.STDOUT)
 
-    im = Image.open(image_temp_file + section_name + '/' + section_name + '-1.jpeg')
+    im = Image.open(image_temp_file + section_name +
+                    '/' + section_name + '-1.jpeg')
     return im
 
 
 def get_data_from_image(im, parser, section_name):
     my_data = {}
     resolution = im.size[1]
-    logger.debug("resolution: " + str(resolution) + " for section: " + section_name)
-    
+    logger.debug("resolution: " + str(resolution) +
+                 " for section: " + section_name)
+
     if parser.has_option(section_name, "same_as"):
         section_name = parser.get(section_name, "same_as")
-    
+
     texts = ['l_name', 'r_name']
     for name in texts:
         if parser.has_option(section_name, name + "_l_" + str(resolution)):
             my_data[name] = get_image(section_name, im, name, "name")
-            
+
     supplies = ['l_supply', 'r_supply']
     for supply in supplies:
         if parser.has_option(section_name, supply + "_l_" + str(resolution)):
             my_data[supply] = get_image(section_name, im, supply, "supply")
-            
+
     if parser.has_option(section_name, "time_l_" + str(resolution)):
         my_data['time'] = get_image(section_name, im, "time", "time")
 
@@ -169,15 +176,15 @@ def get_players(stream_obj):
 
 def game_live(im, data):
     num_valid_fields = 0
-    
+
     for field in ['l_name', 'r_name']:
         if len(data[field]) >= 3:
             num_valid_fields += 3
-    
+
     for field in ['l_supply', 'r_supply']:
-        if re.search(r'\d+/\d+',data[field]):
+        if re.search(r'\d+/\d+', data[field]):
             num_valid_fields += 1
-            
+
     for field in ['l_minerals', 'r_minerals', 'l_gas', 'r_gas', 'l_score', 'r_score']:
         if data[field] >= 0:
             num_valid_fields += 1
@@ -205,37 +212,41 @@ def set_up_bracket(section_name, stream_obj):
 def save_images(section_name):
     for category in ["score", "name", "gas", "minerals", "supply"]:
         for prefix in ["l_", "r_"]:
-            name = ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(10))
+            name = ''.join(random.choice(
+                string.ascii_lowercase + string.digits) for _ in range(10))
             path = "/home/sc2ls/pics/" + section_name + "/" + category
             mkdir_p(path)
             copyfile(image_temp_file + section_name + "/" + prefix + category + '.jpeg',
                      path + "/" + name + ".jpeg")
-    name = ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(10))
+    name = ''.join(random.choice(string.ascii_lowercase + string.digits)
+                   for _ in range(10))
     path = "/home/sc2ls/pics/" + section_name + "/time"
     mkdir_p(path)
     copyfile(image_temp_file + section_name + "/time" + '.jpeg',
-                     path + "/" + name + ".jpeg")
+             path + "/" + name + ".jpeg")
 
 
 def get_info_from_stream(section_name):
     tries = 0
     while True:
-        # While setting up, dissable memory logging
-#         if section_name == "iem":
-#             gc.collect()
-#             print("NEW GRAPH - " + time.strftime("%c"))
-#             objgraph.show_most_common_types()
+        if section_name == "iem":
+            gc.collect()
+            logger.debug("NEW GRAPH - " + time.strftime("%c"))
+            graphOutput = StringIO()
+            objgraph.show_most_common_types(file=graphOutput)
+            logger.debug(graphOutput.getvalue())
         try:
             # Reload parser vars
             #parser = ConfigParser.ConfigParser()
-            #parser.read(sets.conf_file)
-            
+            # parser.read(sets.conf_file)
+
             #logger.debug("Done reading conf for: " + section_name)
-            
+
             stream_url = parser.get(section_name, 'stream_url')
             stream_obj = Stream.objects.filter(url=stream_url)
             if not stream_obj:
-                stream_obj = Stream(url=stream_url, name=parser.get(section_name, 'stream_name'))
+                stream_obj = Stream(url=stream_url, name=parser.get(
+                    section_name, 'stream_name'))
                 stream_obj.save()
             else:
                 stream_obj = stream_obj[0]
@@ -255,7 +266,8 @@ def get_info_from_stream(section_name):
                     tries += 1
                     time.sleep(5)
                     continue
-                logger.info("There doesn't seem to be any stream available for: " + stream_url)
+                logger.info(
+                    "There doesn't seem to be any stream available for: " + stream_url)
                 tries = 0
                 stream_obj.up = False
                 stream_obj.save()
@@ -263,13 +275,15 @@ def get_info_from_stream(section_name):
                 continue
             else:
                 tries = 0
-            
+
             headers = {'Client-ID': 'jy4zwuphqfdvh2nfygxkzb66z23wjz',
                        'Accept': 'application/vnd.twitchtv.v3+json'}
-            res = requests.get("https://api.twitch.tv/kraken/channels/" + stream_url, headers=headers);
+            res = requests.get(
+                "https://api.twitch.tv/kraken/channels/" + stream_url, headers=headers)
             json_res = res.json()
             if json_res["game"] != "StarCraft II":
-                logger.info("Current game is not SCII for stream: " + stream_url)
+                logger.info(
+                    "Current game is not SCII for stream: " + stream_url)
                 stream_obj.up = False
                 stream_obj.save()
                 time.sleep(60)
@@ -280,10 +294,10 @@ def get_info_from_stream(section_name):
                 stream_obj.save()
                 time.sleep(60)
                 continue
-                        
+
             stream_obj.up = True
             stream_obj.save()
-            
+
             # Get game objects
             game_objects = Game.objects.filter(stream=stream_obj)
             if not game_objects:
@@ -295,20 +309,19 @@ def get_info_from_stream(section_name):
             im = get_screenshot(stream_data, section_name)
 
             my_data = get_data_from_image(im, parser, section_name)
-            
+
             for key in my_data.keys():
                 logger.debug(str(key) + ": " + str(my_data[key]))
 
             players = get_players(stream_obj)
-            
-                
+
             if 'l_name' not in my_data:
                 logger.info("No correct conf for: " + stream_url)
                 stream_obj.up = False
                 stream_obj.save()
                 time.sleep(60)
                 continue
-            
+
             if not game_live(im, my_data):
                 if game.game_on:
                     game.game_off_time = timezone.now()
@@ -319,20 +332,21 @@ def get_info_from_stream(section_name):
                 continue
             else:
                 game.game_on = True
-            
+
 #             save_images(section_name)
 
             p_l = players[0]
             p_r = players[1]
 
-            categories = ['name', 'score', 'supply', 'minerals', 'gas', 'workers', 'army']
+            categories = ['name', 'score', 'supply',
+                          'minerals', 'gas', 'workers', 'army']
             for category in categories:
                 if 'l_' + category in my_data:
                     setattr(p_l, category, my_data['l_' + category])
                     setattr(p_r, category, my_data['r_' + category])
             p_l.save()
             p_r.save()
-            
+
             # Game stuff
             if 'time' in my_data:
                 game.current_time = my_data['time']
@@ -340,13 +354,14 @@ def get_info_from_stream(section_name):
                 game.on_map = my_data['map']
 
             game.save()
-    
+
             logger.info("Done with loop for: " + stream_url)
             time.sleep(2)
         except KeyboardInterrupt:
             exit()
         except Exception as _:
-            logger.error("Something went wrong for stream: " + stream_url, exc_info=True)
+            logger.error("Something went wrong for stream: " +
+                         stream_url, exc_info=True)
 
 
 def get_stream_info_thread():
@@ -367,4 +382,3 @@ if __name__ == "__main__":
         logger.debug("Starting thread for " + section)
         t = threading.Thread(name=section, target=get_stream_info_thread)
         t.start()
-
