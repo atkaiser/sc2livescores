69290f3bc3272b454d3eaedd7f5106971c6b78c4
Alex Kaiser <alextkaiser@gmail.com>
2017-12-14 07:53:58 -0800

    Fixing up basetrade tv confs

--- sc2game/utils/test_image.py
+++ sc2game/utils/test_image.py
@@ -1,29 +1,34 @@
-import update_state
-import sys
 import ConfigParser
-from sc2livescores import sets
+import sys
+
 from PIL import Image
 
+# from sc2livescores import sets
+import update_state
+
+
 if len(sys.argv) < 2:
-    print "Use test_image.py <stream name> <image to test on>"
+    print "Use test_image.py <display_type> <image to test on>"
     sys.exit()
 
 section_name = sys.argv[1]
 
 if len(sys.argv) == 3:
     im = Image.open(sys.argv[2])
-else:
-    parser = ConfigParser.ConfigParser()
-    parser.read(sets.conf_file)
-    stream_url = parser.get(section_name, 'stream_url')
-    stream = update_state.get_stream(stream_url)
-    im = update_state.get_screenshot(stream, section_name)
+# else:
+    # TODO this needs to be redone
+#     parser = ConfigParser.ConfigParser()
+#     parser.read(sets.conf_file)
+#     stream_url = parser.get(section_name, 'stream_url')
+#     stream = update_state.get_stream(stream_url)
+#     im = update_state.get_screenshot(stream, section_name)
 
 resolution = im.size[1]
 print "resolution: " + str(resolution) + " for section: " + section_name
-my_data = update_state.get_data_from_image(im, update_state.parser, section_name)
+my_data = update_state.get_data_from_image(im, section_name)
 
 for key in my_data.keys():
     print str(key) + ": " + str(my_data[key])
-    
-print update_state.game_live(im, my_data)
+
+print update_state.game_live(my_data)
+print update_state.get_score(my_data)
